5c5199dcaeb443f7e32770202f9536f6984f9477
Felipe Guntzel <engenharia18@zagonel.com.br>
2026-09-21 10:30:58 -0300

    feat: adicionar descricao de fluxo, recolhimento de etapas e estilizar troca de fluxo

--- functions/api/fluxos/[id].js
+++ functions/api/fluxos/[id].js
@@ -1,5 +1,6 @@
 import { crudItemHandlers } from "../../_lib/crud.js";
 export const { onRequestGet, onRequestPut, onRequestDelete } = crudItemHandlers("fluxo_templates", {
   required: ["nome"],
+  optional: ["descricao"],
   tela: "fluxos",
 });
---
+++ migrations/0014_fluxo_templates_descricao.sql
@@ -0,0 +1,2 @@
+-- Adicionar coluna descricao na tabela fluxo_templates
+ALTER TABLE fluxo_templates ADD COLUMN descricao TEXT;
